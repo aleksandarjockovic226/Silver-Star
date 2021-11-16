55677d3724af097c230ed688c9c7ff32f880c9c1
aleksandarjockovic226 <aleksandarjockovic226@gmail.com>
2021-11-16 13:15:25 +0100

    contact form validation using 'useRef'

--- src/components/pages/home/contact.jsx
+++ src/components/pages/home/contact.jsx
@@ -1,10 +1,50 @@
-import React from 'react';
-
+import React, { useRef, useState } from 'react';
 import SendSvg from '../../../img/send.svg';
 
 
-export default function Contact(){
-    return(
+
+function Contact() {
+    const name = useRef();
+    const email = useRef();
+    const phone = useRef();
+    const message = useRef();
+
+    const [nameErr, setNameErr] = useState("")
+    const [emailErr, setEmailErr] = useState("")
+    const [phoneErr, setPhoneErr] = useState("")
+    const [messageErr, setMessageErr] = useState("")
+
+    const [sentMessage, setSentMessage] = useState("")
+
+    let regexEmail = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    let regexPhone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+
+
+    const handleSend = (e) => {
+        e.preventDefault();
+        if (name.current.value === "" || name.current.value.length < 3) {
+            const nameErrText = "Unesite ime sa minimum 3 karaktera"
+            setNameErr(nameErrText)
+        } else if (email.current.value === "" || !regexEmail.test(email.current.value)) {
+            const emailErrText = "Unesite ispravnu email adresu"
+            setEmailErr(emailErrText)
+        } else if (phone.current.value === "" || !regexPhone.test(phone.current.value)) {
+            const phoneErrText = "Unesite ispravan broj telefona"
+            setPhoneErr(phoneErrText)
+        } else if (message.current.value === "" || message.current.value.length < 9) {
+            const messageErrText = "Unesite poruku sa minimum 9 karaktera"
+            setMessageErr(messageErrText)
+        } else {
+            setNameErr("")
+            setEmailErr("")
+            setPhoneErr("")
+            setMessageErr("")
+            
+            setSentMessage("Vaša poruka je poslata.")
+        }
+    }
+    return (
         <div id="contact">
             <div className="left">
                 <p className="title">Kontakt</p>
@@ -13,13 +53,47 @@ export default function Contact(){
             <div className="right">
                 <p className="title">Kontaktirajte Nas</p>
                 <form>
-                    <input type="text" name="name" placeholder="Ime" />
-                    <input type="mail" name="mail" placeholder="Email" />
-                    <input type="phone" name="phone" placeholder="Telefon" />
-                    <textarea name="message" placeholder="Poruka"></textarea>
-                    <button>Pošaljite<img src={SendSvg} alt="send" /></button>
+                    <label htmlFor="name">
+                        {nameErr}
+                    </label>
+                    <input
+                        ref={name}
+                        type="text"
+                        name="name"
+                        placeholder="Ime"
+                    />
+                    <label htmlFor="email">
+                        {emailErr}
+                    </label>
+                    <input
+                        ref={email}
+                        type="mail"
+                        name="email"
+                        placeholder="Email"
+                    />
+                    <label htmlFor="phone">
+                        {phoneErr}
+                    </label>
+                    <input
+                        ref={phone}
+                        type="phone"
+                        name="phone"
+                        placeholder="Telefon"
+                    />
+                    <label htmlFor="message">
+                        {messageErr}
+                    </label>
+                    <textarea
+                        ref={message}
+                        name="message"
+                        placeholder="Poruka"
+                    ></textarea>
+
+                    <button name="btn" onClick={handleSend} >Pošaljite<img src={SendSvg} alt="send" /></button>
+                    <label className="labelSent" htmlFor="bt">{sentMessage}</label>
                 </form>
             </div>
         </div>
     )
 }
+export default Contact
